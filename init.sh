3d5fe6b91eaf15698fc79925c015174dd8aa9e24
Phillip-England <phillip.mark.england@gmail.com>
2025-12-24 09:55:18 -0600

    patch: init.sh was out of date

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ echo "ADMIN_ID=1323123212123123" >> .env
 echo "SQLITE_ABSOLUTE_PATH=./main.db" >> .env
 echo "ADMIN_USERNAME=cow" >> .env
 echo "ADMIN_PASSWORD=chicken" >> .env
+echo "TBOT_KEY=asdasdaqweqweqadas" >> .env
 mkdir static
